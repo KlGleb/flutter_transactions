f7861a30f4152d8f5448a5455292dc3bdb500c1e
Gleb Klimov <info@gleb.at>
2023-10-03 16:01:27 +0200

    Fix typed epics

--- lib/core/redux/epics.dart
+++ lib/core/redux/epics.dart
@@ -3,6 +3,8 @@ import 'package:transactions/core/di/init_dependencies.dart';
 import 'package:transactions/core/redux/app_state.dart';
 import 'package:transactions/features/home/presentation/state_management/home_actions.dart';
 import 'package:transactions/features/home/presentation/state_management/home_epics.dart';
+import 'package:transactions/features/transaction_details/presentation/state_management/transaction_details_actions.dart';
+import 'package:transactions/features/transaction_details/presentation/state_management/transaction_details_epics.dart';
 
 final EpicMiddleware<AppState> epicMiddleware = EpicMiddleware(epic);
 
@@ -10,4 +12,4 @@ final epic = combineEpics<AppState>([_subscribeEpic, _unsubscribeEpic, _subscrib
 
 final _subscribeEpic = TypedEpic<AppState, HomeAction>(SubscribeCountEpic(getIt()));
 final _unsubscribeEpic = TypedEpic<AppState, HomeAction>(SubscribeTransactionsEpic(getIt()));
-final _subscribeTransactionEpic = TypedEpic<AppState, HomeAction>(SubscribeTransactionsEpic(getIt()));
+final _subscribeTransactionEpic = TypedEpic<AppState, TransactionDetailsAction>(SubscribeTransactionEpic(getIt()));
